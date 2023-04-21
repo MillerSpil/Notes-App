bf9a07d376a388f648d025fed5b3259a6f896b7b
Miller <53185757+MillerSpil@users.noreply.github.com>
2023-04-20 22:48:09 -0500

    Updates 4/20/23 - 10:48PM
    -Refactoring code for ease of use
    -Added the ability to edit notes

--- main.js
+++ main.js
@@ -9,25 +9,34 @@ canvas.height = window.innerHeight;
 // This is the array that holds all the notes
 const notes = [];
 
+// This is the function that is called to open the note form
+function openNoteForm(title = '', text = '', color = '#FF4A4A') {
+  document.getElementById('noteTitle').value = title;
+  document.getElementById('noteText').value = text;
+  document.getElementById('noteColor').value = color;
+  document.getElementById('noteForm').style.display = 'flex';
+  return new Promise((resolve, reject) => {
+    document.getElementById('noteSubmit').addEventListener('click', (event) => {
+      event.preventDefault();
+      const title = document.getElementById('noteTitle').value;
+      const text = document.getElementById('noteText').value;
+      const color = document.getElementById('noteColor').value;
+      if (title && text && color) {
+        resolve({ title, text, color });
+        document.getElementById('noteForm').style.display = 'none';
+        document.getElementById('noteTitle').value = '';
+        document.getElementById('noteText').value = '';
+        document.getElementById('noteColor').value = '#FF4A4A';
+      }
+    });
+  });
+}
+
 // This is the event listener to show the add note form
 document.getElementById('addNote').addEventListener('click', () => {
-  document.getElementById('addNoteForm').style.display = 'flex';
-  noteFormSetting = 'add';
-});
-
-// This is the event listener to submit the add note form or the edit note form
-document.getElementById('noteSubmit').addEventListener('click', (event) => {
-  event.preventDefault();
-  const title = document.getElementById('noteTitle').value;
-  const text = document.getElementById('noteText').value;
-  const color = document.getElementById('noteColor').value;
-  if (title && text && color) {
-    Note.newNote(title, text, color);
-    document.getElementById('addNoteForm').style.display = 'none';
-    document.getElementById('noteTitle').value = '';
-    document.getElementById('noteText').value = '';
-    document.getElementById('noteColor').value = '#FF4A4A';
-  }
+  openNoteForm().then((note) => {
+    Note.newNote(note.title, note.text, note.color);
+  });
 });
 
 // This is the function that is called to confirm a change to a note
@@ -90,20 +99,23 @@ class Note {
     const note = new Note(x, y, 200, 200, color, title, text);
     notes.push(note);
   }
-  
+
+  //static function to get the top most note at a given x and y
+  static getNoteAt(x, y) {
+    for (let i = notes.length - 1; i >= 0; i--) {
+      const note = notes[i];
+      if (x > note.x && x < note.x + note.width && y > note.y && y < note.y + note.height) {
+        return note;
+      }
+    }
+    return null;
+  }
 }
 
 // This is for dragging the notes around
 canvas.addEventListener('mousedown', e => {
   const mouse = { x: e.clientX, y: e.clientY };
-  let topmostNote = null;
-  for (let i = notes.length - 1; i >= 0; i--) {
-    const note = notes[i];
-    if (mouse.x > note.x && mouse.x < note.x + note.width && mouse.y > note.y && mouse.y < note.y + note.height) {
-      topmostNote = note;
-      break;
-    }
-  }
+  let topmostNote = Note.getNoteAt(mouse.x, mouse.y);
   if (topmostNote) {
     let dx = mouse.x - topmostNote.x, dy = mouse.y - topmostNote.y;
     const onDrag = e => {
@@ -117,13 +129,8 @@ canvas.addEventListener('mousedown', e => {
 
 // This is for double clicking on a note to bring it to the front
 canvas.addEventListener('dblclick', (e) => {
-  const { clientX: x, clientY: y } = e;
-  const topmostNote = notes.find((note) =>
-    x > note.x &&
-    x < note.x + note.width &&
-    y > note.y &&
-    y < note.y + note.height
-  );
+  const mouse = { x: e.clientX, y: e.clientY };
+  let topmostNote = Note.getNoteAt(mouse.x, mouse.y);
   if (topmostNote) {
     notes.splice(notes.indexOf(topmostNote), 1);
     notes.push(topmostNote);
@@ -205,25 +212,31 @@ class CustomContextMenu {
 
 // This is a context menu that is used for many functions in the app
 const menu = new CustomContextMenu([
-  { label: 'Delete Note', action: () => deleteNote() }
+  { label: 'Delete Note', action: () => deleteNote() },
+  { label: 'Edit Note', action: () => editNote() },
 ]);
 
 // This is the function that is called when the user clicks on the
 // delete note button
 async function deleteNote() {
   const mouse = { x: event.clientX, y: event.clientY };
-  let topmostNote = null;
-  for (let i = notes.length - 1; i >= 0; i--) {
-    const note = notes[i];
-    if (mouse.x > note.x && mouse.x < note.x + note.width && mouse.y > note.y && mouse.y < note.y + note.height) {
-      topmostNote = note;
-      break;
-    }
-  }
+  let topmostNote = Note.getNoteAt(mouse.x, mouse.y);
   if (topmostNote) {
     const confirmed = await confirmChange();
     if (confirmed) {
       notes.splice(notes.indexOf(topmostNote), 1);
     }
+  }
+}
+
+function editNote() {
+  const mouse = { x: event.clientX, y: event.clientY };
+  let topmostNote = Note.getNoteAt(mouse.x, mouse.y);
+  if (topmostNote) {
+    openNoteForm(topmostNote.title, topmostNote.text, topmostNote.color).then((note) => {
+      topmostNote.title = note.title;
+      topmostNote.text = note.text;
+      topmostNote.color = note.color;
+    });
   }
 }
